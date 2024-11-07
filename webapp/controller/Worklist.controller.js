d5fceeb28a80b727c940727c6cc52f9aa7cbef00
IngaMuse <ingamoshkareva@gmail.com>
2024-11-07 23:35:14 +0300

    feat: clean from unnessesary code

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -12,22 +12,11 @@ sap.ui.define([
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
-
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
 			onInit : function () {
 				var oViewModel,
 					iOriginalBusyDelay,
 					oTable = this.byId("table");
 
-				// Put down worklist table's original value for busy indicator delay,
-				// so it can be restored later on. Busy handling on the table is
-				// taken care of by the table itself.
 				iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
 				// keeps the search state
 				this._aTableSearchState = [];
@@ -51,20 +40,6 @@ sap.ui.define([
 					oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
 				});
 			},
-
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
-
-			/**
-			 * Triggered by the table's 'updateFinished' event: after new table
-			 * data is available, this handler method updates the table counter.
-			 * This should only happen if the update was successful, which is
-			 * why this handler is attached to 'updateFinished' and not to the
-			 * table's list binding's 'dataReceived' method.
-			 * @param {sap.ui.base.Event} oEvent the update finished event
-			 * @public
-			 */
 			onUpdateFinished : function (oEvent) {
 				// update the worklist's object counter after the table update
 				var sTitle,
@@ -80,21 +55,12 @@ sap.ui.define([
 				this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
 			},
 
-			/**
-			 * Event handler when a table item gets pressed
-			 * @param {sap.ui.base.Event} oEvent the table selectionChange event
-			 * @public
-			 */
+
 			onPress : function (oEvent) {
 				// The source is the list item that got pressed
 				this._showObject(oEvent.getSource());
 			},
 
-			/**
-			 * Event handler for navigating back.
-			 * We navigate back in the browser historz
-			 * @public
-			 */
 			onNavBack : function() {
 				history.go(-1);
 			},
@@ -102,10 +68,6 @@ sap.ui.define([
 
 			onSearch : function (oEvent) {
 				if (oEvent.getParameters().refreshButtonPressed) {
-					// Search field's 'refresh' button has been pressed.
-					// This is visible if you select any master list item.
-					// In this case no new search is triggered, we only
-					// refresh the list binding.
 					this.onRefresh();
 				} else {
 					var aTableSearchState = [];
@@ -119,37 +81,19 @@ sap.ui.define([
 
 			},
 
-			/**
-			 * Event handler for refresh event. Keeps filter, sort
-			 * and group settings and refreshes the list binding.
-			 * @public
-			 */
+
 			onRefresh : function () {
 				var oTable = this.byId("table");
 				oTable.getBinding("items").refresh();
 			},
 
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-
-			/**
-			 * Shows the selected item on the object page
-			 * On phones a additional history entry is created
-			 * @param {sap.m.ObjectListItem} oItem selected Item
-			 * @private
-			 */
 			_showObject : function (oItem) {
 				this.getRouter().navTo("object", {
 					objectId: oItem.getBindingContext().getProperty("HeaderID")
 				});
 			},
 
-			/**
-			 * Internal helper method to apply both filter and search state together on the list binding
-			 * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
-			 * @private
-			 */
+
 			_applySearch: function(aTableSearchState) {
 				var oTable = this.byId("table"),
 					oViewModel = this.getModel("worklistView");
