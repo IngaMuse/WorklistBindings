e0744870fee5766e46e5a1ea011a15e4d5840296
IngaMuse <ingamoshkareva@gmail.com>
2024-11-10 13:33:17 +0300

    feat: added drag and dropp

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -67,7 +67,8 @@ sap.ui.define(
         },
 
         _getTableCounter() {
-          const sCount = this.getView().getModel("jsonModel").getData().aData.length;
+          const sCount = this.getView().getModel("jsonModel").getData()
+            .aData.length;
           this.getModel("worklistView").setProperty("/sCount", sCount);
         },
 
@@ -122,7 +123,9 @@ sap.ui.define(
             oItem = oTable.getSelectedItem(),
             oContext = oItem.getBindingContext("jsonModel"),
             sValue = oEvent.getParameter("value");
-          oContext.getModel("jsonModel").setProperty("Description", sValue, oContext);
+          oContext
+            .getModel("jsonModel")
+            .setProperty("Description", sValue, oContext);
           oContext.getModel("jsonModel").refresh(true);
         },
         onToggleGrouping: function () {
@@ -144,7 +147,53 @@ sap.ui.define(
           oBinding.sort(aSorters);
         },
 
-        onDropSelectedProductsTable: function () {},
+        onDropSelectedItems: function (oEvent) {
+          const oDraggedControl = oEvent.getParameter("draggedControl");
+          const oDroppedOnControl = oEvent.getParameter("droppedControl");
+          const dropPosition = oEvent.getParameter("dropPosition");
+          if (!oDraggedControl || !oDroppedOnControl) {
+            console.error("Invalid dragged or dropped control");
+            return;
+          }
+          const oModel = this.getView().getModel("jsonModel");
+          let aItems = oModel.getProperty("/aData");
+
+          const oDraggedBindingContext =
+            oDraggedControl.getBindingContext("jsonModel");
+          const oDroppedBindingContext =
+            oDroppedOnControl.getBindingContext("jsonModel");
+
+          if (!oDraggedBindingContext || !oDroppedBindingContext) {
+            console.error(
+              "Binding context is undefined for dragged or dropped control"
+            );
+            return;
+          }
+
+          const draggedIndex = aItems.findIndex(
+            (item) => item.Order === oDraggedBindingContext.getObject().Order
+          );
+          const droppedIndex = aItems.findIndex(
+            (item) => item.Order === oDroppedBindingContext.getObject().Order
+          );
+          const [movedItem] = aItems.splice(draggedIndex, 1);
+
+          if (dropPosition === "Between") {
+            aItems.splice(droppedIndex + 1, 0, movedItem);
+          } else {
+            aItems.splice(droppedIndex, 0, movedItem);
+          }
+          if (draggedIndex < droppedIndex) {
+            for (let i = draggedIndex; i <= droppedIndex; i++) {
+                aItems[i].Order = i + 1;
+            }
+        } else {
+            for (let i = droppedIndex; i <= draggedIndex; i++) {
+                aItems[i].Order = i + 1;
+            }
+          }
+          oModel.setProperty("/aData", aItems);
+        },
       }
     );
   }
